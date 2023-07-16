3123cf762c411564b813fcabc24f54af5263957b
CodeSandbox <support@codesandbox.io>
2023-07-16 15:34:54 +0000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React, { ReactElement } from 'react'
+import LSP from './LSP'
+
+function App(): ReactElement {
+  return (
+    <div className="w-full h-full flex justify-center items-center">
+      <LSP />
+    </div>
+  )
+}
+
+export default App
